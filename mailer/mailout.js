95206de23aeff3321f1de30de0451a830bf510ac
OoogleBoogle <simattfield@gmail.com>
2016-05-25 17:06:32 -0700

    Added send grid templates

--- mailer/mailout.js
+++ mailer/mailout.js
@@ -7,7 +7,8 @@ var confirmMail = function(to, firstName) {
   mail.to = to;
   mail.from = 'simattfield@gmail.com',
   mail.subject = 'Hi from EatUp',
-  mail.html = "<h3>Hay " + firstName + "! Your request has been saved. We'll contact you again when we've found you a match.</h3>"
+  mail.text = "Hay " + firstName + "! Your request has been saved. We'll contact you again when we've found you a match.",
+  mail.html = "<h3>Hay " + firstName + "! Your request has been saved. We'll contact you again when we've found you a match.</h3>",
   mail.setFilters({
     'templates': {
       'settings': {
@@ -23,13 +24,23 @@ var confirmMail = function(to, firstName) {
   });
 }
 
+
+
 var pairedMail = function(to, user, pair, place) {
-  var mail = {
-    to: to,
-    from: 'simattfield@gmail.com',
-    subject: "You've been paired by EatUp",
-    text: "Get ready " + user + "! You've been paired up with " + pair + " and you're going to " + place + "."
-  };
+  var mail = new sendgrid.Email();
+  mail.to = to;
+  mail.from = 'simattfield@gmail.com',
+  mail.subject = 'Hi from EatUp',
+  mail.text = "Hay " + user + "! Tonight you'll be heading out to " + place + " with " + pair + "!",
+  mail.html = "<h3>Hay " + user + "! Tonight you'll be heading out to " + place + " with " + pair + "!</h3>",
+  mail.setFilters({
+    'templates': {
+      'settings': {
+        'enable': 1,
+        'template_id': 'e840b823-748d-4473-8e6b-89f90ac35bc5',
+      }
+    }
+  })
 
   sendgrid.send(mail, function(err, json) {
     if (err) { console.error(err); }
